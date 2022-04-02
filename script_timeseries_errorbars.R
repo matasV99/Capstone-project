7788e0f93fe8430c5781dd8b08a7180b42ae29cd
matasV99 <70501272+matasV99@users.noreply.github.com>
2022-04-02 17:55:00 +0800

    Add files via upload

---
+++ script_timeseries_errorbars.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(scales)
+
+# https://docs.google.com/spreadsheets/d/1D2qoPJS2D4l0YotTsV1HLJxbTTjD0m6hTfBJ8s8Pnlk/edit#gid=0
+my_data <- read.table(file = "clipboard", sep = "\t", header = TRUE)
+
+p <- ggplot(my_data, aes(x=Time, y=Averages, color = Condition)) +
+  geom_line() +
+  ylab(label = "Recovery index") +
+  xlab(label = "Time (min)")+
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+  guides(col=guide_legend("Different experimental conditions")) +
+  geom_errorbar(aes(ymin=Averages-Sd, ymax=Averages+Sd), width=.2,
+                position=position_dodge(.5)) +
+  theme_bw()
+
+
+p
